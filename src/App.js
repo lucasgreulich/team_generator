5d90b3562e7f545f7b1bb6d9ebc31bc605d65aa0
Lucas Greulich <lucas.g.greulich@gmail.com>
2024-08-19 16:34:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="list_container">
+        <div className="list_box">
+          <ul className="names_list">Lucas</ul>
+          <ul className="names_list">Adam</ul>
+        </div>
+        <button className="randomization_button">Click</button>
+      </div>
+    </>
   );
 }
 
