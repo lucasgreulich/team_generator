3f5b449fe3979c31e9e3fb89c295ed74d925acf6
Lucas Greulich <lucas.g.greulich@gmail.com>
2024-08-20 16:24:04 -0700

    amended functions to allow for button to show items in a list

--- src/App.js
+++ src/App.js
@@ -4,29 +4,51 @@ import { useState } from 'react';
 
 
 export default function App() {
+  //useStates
   const [name, setName] = useState("");
+  const [list, setList] = useState([]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert(`The name you entered was: ${name}`)
+  //handling change 
+  function handleInputChange(e) {
+    setName(e.target.value);
+  };
+  
+  //handling submit
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({name});
+
+    if(name.trim() !== '' ){
+      setList([...list, name]);
+      setName('');
+    }
   };
 
+  //handle clearing the list 
+  function handleClear() {
+    setList([]);
+  };
 
   return (
     <>
       <div className="names_box">
-        <form onSubmit={handleSubmit}>
-          <h1>Add names</h1>
-          <label htmlFor='title'>Enter Names: 
+        <form className='form_one' onSubmit={handleSubmit}>
+          <h1>Add names</h1> 
           <input 
           type='text' 
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={handleInputChange}
+          placeholder='Enter Name'
           />
-          </label>
-          
           <button className="button" type='submit'>Add</button>
+          <button className='button' onClick={handleClear}>Clear</button>
         </form>
+
+        <ul className='list'>
+          {list.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
       </div>
 
       <div className="team_generator_container">
